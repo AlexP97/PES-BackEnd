a630ff4d23eb409fc81a02c755a1eab9a0980012
Alejandro Pérez Ortiz <alejandro.perez.ortiz@est.fib.upc.edu>
2018-04-17 16:47:14 +0200

    Edit profile

---
+++ peticiones_php/edit_profile.php
@@ -0,0 +1,33 @@
+<?php
+    require 'connection.php';
+
+		//return;
+
+
+    $username = $_POST["username"];
+	$password = hash('sha256', $_POST["user_password"]."AssistMe");
+	$email = $_POST["email"];
+	//$usertype = $_POST["usertype"];
+	$name = $_POST["name"];
+	$surname = $_POST["surname"];
+	$country = $_POST["country"];
+
+	header('Content-type: application/json');
+
+	$queryUpdateProfile = "UPDATE username SET password = '" . $password . "', email = '" . $email . "', name = '" . $name . "', surname = '" . $surname . "', country = '" . $country . "';";
+
+	$result = $conn->query($queryUpdateProfile);
+
+	if($result === TRUE) {
+		$res->correct = true;
+		$res->result = "Se ha modificado el perfil correctamente.";
+		$myJSON = json_encode($res);
+		echo $myJSON;
+	}
+	else {
+	    $res->correct = false;
+		$res->result = "Error desconocido.";
+		$myJSON = json_encode($res);
+		echo $myJSON;
+	}
+?>
