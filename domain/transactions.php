ed76441d2d120e685f840a3d7e654d6683f7205c
root <root@localhost.localdomain>
2018-05-03 16:09:39 +0200

    Refactor at transaction.php -> applyed template pattern. still pending implement a session (check sessions for api rest)

--- domain/transactions.php
+++ domain/transactions.php
@@ -7,7 +7,24 @@ require_once 'factory.php';
 */
 abstract class Transaction
 {
-	abstract protected function execute();
+	protected $parameters; 
+	protected $response;
+
+	abstract protected function checkParameters();
+	abstract protected function processRequest();
+
+	public function execute() 
+	{
+		$this->parameters = array('valid'	 =>		true, 	//false -> invalid response
+								  'session'  =>		0,			// 0 -> guest, 1-> normal 2-> admin
+								  'error'	 =>		"ok");
+		$this->checkParameters();
+		$this->response = new \stdClass();
+		$this->processRequest();
+		header('Content-type: application/json');
+		$myJSON = json_encode($this->response);
+		echo $myJSON;
+	}
 }
 
 /**
@@ -17,7 +34,6 @@ class LoginRequest extends Transaction
 {
 	private $username;
 	private $password;
-	private $response;
 
 	function __construct()
 	{
@@ -25,18 +41,28 @@ class LoginRequest extends Transaction
 		$this->username = isset($_POST["username"]) ? $_POST["username"] : null;
 		$this->password = isset($_POST["password"]) ? $_POST["password"] : null;
 		if($this->password !== null) $this->password = hash('sha256', $this->password."AssistMe");
-		$this->response = new \stdClass();
 	}
 
-	public function execute() 
+	public function checkParameters()
 	{
-		if($this->username === null || $this->password === null) return json_encode("false");
-		$this->response->correct 
-			= SingletonDataFactory::getInstance()->getUserDBController()->validLogin($this->username,
+		if($this->username === null || $this->password === null) {
+			$this->parameters['valid'] = false;
+			$this->parameters['error'] = "invalid null parameter on uri";
+		}
+
+	}
+	
+	public function processRequest()
+	{
+		if(!$this->parameters['valid']) {
+			$this->response->error = $this->parameters['error'];
+			$this->response->correct = "false";
+		}
+		else {
+			$this->response->correct 
+				= SingletonDataFactory::getInstance()->getUserDBController()->validLogin($this->username,
 				$this->password);
-		header('Content-type: application/json');
-		$myJSON = json_encode($this->response);
-		echo $myJSON;
+		}
 	}
 }
 
@@ -51,7 +77,6 @@ class EditUserRequest extends Transaction
 	private $name;
 	private $surname;
 	private $country;
-	private $response;
 
 	function __construct()
 	{
@@ -62,12 +87,15 @@ class EditUserRequest extends Transaction
 		$this->name = $_POST["name"];
 		$this->surname = $_POST["surname"];
 		$this->country = $_POST["country"];
-		$this->response = new \stdClass();
 	}
 
-	public function execute() 
+	public function checkParameters()
 	{
-		header('Content-type: application/json');
+
+	}
+
+	public function processRequest()
+	{
 		$data = array(
 			"username" => $this->username,
 			"password" => $this->password,
@@ -76,17 +104,16 @@ class EditUserRequest extends Transaction
 			"surname" => $this->surname,
 			"country" => $this->country,
 		);
+
 		$result = SingletonDataFactory::getInstance()->getUserDBController()->editUser($data);
 		 if($result==="true") {
-		 	$this->$response->correct = "true";
-			$this->$response->result = "Se ha modificado el perfil correctamente.";
+		 	$this->response->correct = "true";
+			$this->response->result = "Se ha modificado el perfil correctamente.";
 		 }
 		 else {
 		 	$this->response->correct = "false";
 		 	$this->response->result = $result;
 		 }
-		$myJSON = json_encode($this->response);
-		echo $myJSON;
 	}
 }
 
@@ -100,10 +127,17 @@ class UserRequest extends Transaction
 	{
 		# code...
 	}
-	public function execute()
+
+	public function checkParameters()
 	{
 
 	}
+
+	public function processRequest()
+	{
+
+	}
+
 }
 
 /**
@@ -118,7 +152,6 @@ class RegisterRequest extends Transaction
 	private $name;
 	private $surname;
 	private $country;
-	private $resposne;
 	private $error;
 
 	function __construct()
@@ -132,18 +165,19 @@ class RegisterRequest extends Transaction
 		$this->surname = isset($_POST["surname"]) ? $_POST["surname"] : null;
 		$this->country = isset($_POST["country"]) ? $_POST["country"] : null;
 
-		$this->response = new \stdClass();
-
 		$this->error = FALSE;
 	}
 
-	public function execute() 
+	public function checkParameters()
 	{
-		if($this->username === null || $this->password === null || $this->email === null) {
+				if($this->username === null || $this->password === null || $this->email === null) {
 			$this->response->correct = "false";
 			$this->response->result = "se han enviado campos obligatorios nulos";
 		}
-		else {
+	}
+
+	public function processRequest()
+	{
 			$data = array(
 			"username" => $this->username,
 			"password" => $this->password,
@@ -162,10 +196,6 @@ class RegisterRequest extends Transaction
 		 	$this->response->correct = "false";
 		 	$this->response->result = $result;
 		 }
-		}
-		header('Content-type: application/json');
-		$myJSON = json_encode($this->response);
-		echo $myJSON;
 	}
 }
 
@@ -180,7 +210,12 @@ class GuideRequest extends Transaction
 		# code...
 	}
 
-	public function execute()
+	public function checkParameters()
+	{
+
+	}
+
+	public function processRequest()
 	{
 
 	}
@@ -189,17 +224,12 @@ class GuideRequest extends Transaction
 /**
 * 
 */
-class DTOGuideRequest extends Transaction
+class DTOGuideRequest 
 {
 	
 	function __construct()
 	{
 		# code...
-	}
-
-	public function execute()
-	{
-
 	}
 }
 
@@ -212,24 +242,24 @@ class NewGuideRequest extends Transaction
 	private $username;
 	private $data;
 	private $title;
-	private $response;
 	
 	function __construct()
 	{
 		$this->username = isset($_POST["username"]) ? $_POST["username"] : null;
 		$this->data = isset($_POST["data"]) ? $_POST["data"] : null;
 		$this->title = isset($_POST["title"]) ? $_POST["title"] : null;
-		$this->response = new \stdClass();
 	}
 
-	public function execute()
+	public function checkParameters()
 	{
-		header('Content-type: application/json');
+
+	}
+
+	public function processRequest()
+	{
 		$this->response->correct 
 			= SingletonDataFactory::getInstance()->getGuideDBController()->insertGuide($this->username,
-				$this->data, $this->title);
-		$myJSON = json_encode($this->response);
-		echo $myJSON;
+			$this->data, $this->title);
 	}
 }
 
@@ -240,21 +270,21 @@ class GetGuidesRequest extends Transaction
 {
 
 	private $username;
-	private $response;
 	
 	function __construct()
 	{
 		$this->username = isset($_GET["username"]) ? $_GET["username"] : null;
-		$this->response = new \stdClass();
 	}
 
-	public function execute()
+	public function checkParameters()
+	{
+
+	}
+
+	public function processRequest()
 	{
 		$this->response
 			= SingletonDataFactory::getInstance()->getGuideDBController()->getTitlesGuides($this->username);
-		header('Content-type: application/json');
-		$myJSON = json_encode($this->response);
-		echo $myJSON;
-	}
+	} 
 }
 
